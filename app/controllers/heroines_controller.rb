67f2ddc0820db445e2a85a11bef3fe1489fab9d8
jequezinho <jroverby92@gmail.com>
2020-03-20 11:48:15 -0500

    First commit... for my C-minus level product

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -1,5 +1,48 @@
 class HeroinesController < ApplicationController
+
   def index
     @heroines = Heroine.all
   end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def new
+    @heroine = Heroine.new
+    @powers = Power.all
+  end
+
+  def create
+    @heroine = Heroine.new(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @powers = Power.all
+      render :new
+    end
+  end
+
+  def edit
+    @heroine = Heroine.find(params[:id])
+    @powers = Power.all
+  end
+
+  def update
+    @heroine = Heroine.find(params[:id])
+    @heroine.assign_attributes(heroine_params)
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      @powers = Power.all
+      render :edit
+    end
+  end
+
+  private
+
+  def heroine_params
+    params.require(:heroine).permit(:name, :super_name)
+  end
+
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,45 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def new
+    @power = Power.new
+    @heroines = Heroine.all
+  end
+
+  def create
+    @power = Power.new(power_params)
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      @heroines = Heroine.all
+      render :new
+    end
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+    @heroines = Heroine.all
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.assign_attributes(power_params)
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end
+
 end
