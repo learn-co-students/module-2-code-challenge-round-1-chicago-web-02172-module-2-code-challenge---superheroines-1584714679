7517ea9c0831e9bb086a0dfa7c60b99b1e51f57c
owenabbott <ocabbott@gmail.com>
2020-03-20 11:47:49 -0500

    I don't understand anything.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   resources :powers, only: [:index]
   resources :heroines, only: [:index]  
+
+  get '/heroines/index', to: 'heroines#index'
+  get '/powers/index', to: 'powers#index'
+  get 'heroines/:id', to: 'heroines#show', as: "heroine"
+
 end
