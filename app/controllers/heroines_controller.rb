0e73130d6dfedc3c57b919c9cf90c1b5b6432b40
deber116 <deber116@gmail.com>
2020-03-20 11:48:37 -0500

    David E finished code challenge

--- app/controllers/heroines_controller.rb
+++ app/controllers/heroines_controller.rb
@@ -2,4 +2,27 @@ class HeroinesController < ApplicationController
   def index
     @heroines = Heroine.all
   end
+
+  def new
+    @heroine = Heroine.new
+  end
+
+  def show
+    @heroine = Heroine.find(params[:id])
+  end
+
+  def create
+    @heroine = Heroine.new(name: strong_params[:name], super_name:strong_params[:super_name])
+    power = Power.find(strong_params[:powers])
+    @heroine.powers << power
+    if @heroine.save
+      redirect_to heroine_path(@heroine)
+    else
+      render :new
+    end
+  end
+  
+  def strong_params
+    params.require(:heroine).permit(:name, :super_name, :powers)
+  end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,31 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    @power = Power.find(params[:id])
+  end
+
+  def edit
+    @power = Power.find(params[:id])
+  end
+
+  def update
+    @power = Power.find(params[:id])
+    @power.assign_attributes(name:strong_params[:name], description:strong_params[:description])
+    heroines = params[:power][:heroine_ids]
+    heroines.shift
+    heros = heroines.map {|h| Heroine.find(h.to_i)}
+    @power.heroines << heros
+    if @power.save
+      redirect_to power_path(@power)
+    else
+      render :edit
+    end
+  end
+
+  def strong_params
+    params.require(:power).permit(:name, :description, heroine_ids:[])
+  end
+
 end
