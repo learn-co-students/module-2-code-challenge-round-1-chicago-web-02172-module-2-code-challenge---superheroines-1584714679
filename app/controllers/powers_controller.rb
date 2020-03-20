23cfcd4163e8414fea1052cf59f7018a3df2e6b6
cooljasonmelton <cooljasonmelton>
2020-03-20 11:47:14 -0500

    code challenge

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -2,4 +2,38 @@ class PowersController < ApplicationController
   def index
     @powers = Power.all
   end
+
+  def show
+    find_power  
+  end 
+
+  def edit
+    @heroines = Heroine.all
+    find_power
+  end 
+
+  def update
+    find_power.assign_attributes(power_params)
+    @heroines = Heroine.all
+    if @power.valid?
+      @power.save
+      redirect_to power_path(@power)
+    else
+      @errors = @power.errors.full_messages
+      render :new 
+    end 
+
+  end 
+
+  private
+  def find_power
+    @power = Power.find(params[:id])
+  end
+
+  def power_params
+    params.require(:power).permit(:name, :description)
+  end 
+
+
+
 end
--- app/models/power.rb
+++ app/models/power.rb
@@ -1,2 +1,11 @@
 class Power < ApplicationRecord
+    has_many :heroine_powers
+    has_many :heroines, through: :heroine_powers
+
+    def self.num_of_heroines_per_power
+        # Power.all.map
+
+    end 
+
+
 end
