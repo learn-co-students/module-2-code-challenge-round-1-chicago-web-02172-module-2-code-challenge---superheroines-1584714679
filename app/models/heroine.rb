bc126e97fc60999daf32fec2f755c2bfb5c15d04
ellisandrews <ellisandrews1@gmail.com>
2020-03-20 10:54:15 -0500

    Step 5-7

--- app/models/heroine.rb
+++ app/models/heroine.rb
@@ -1,4 +1,7 @@
 class Heroine < ApplicationRecord
     has_many :heroine_powers
     has_many :powers, through: :heroine_powers
+
+    validates :name, presence: true 
+    validates :super_name, { presence: true, uniqueness: true }
 end
